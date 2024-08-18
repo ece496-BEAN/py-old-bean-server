d4276ac046d0a1fe290f9c3f78ce2c84fc53a63f
Fred Chen <chenfred9898@gmail.com>
2024-08-18 07:30:44 -0400

    build: Initial commit

---
+++ devenv.nix
@@ -0,0 +1,33 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = with pkgs; [ 
+    git # Source code versioning
+    zellij # A better tmux (terminal multiplexer)
+    atuin # Command history, to use it with fish, follow this: https://docs.atuin.sh/guide/installation/#installing-the-shell-plugin
+    helix # A modal editor, similar to vim but different
+    fish # A better shell than bash
+    toybox # Unix command line utilities like which, clear
+    less # file pager
+    curl # Transferring files
+  
+    # python configured below
+  ];
+
+  languages.python = {
+    enable = true;
+    version = "3.12.4";
+    venv.enable = true;
+    venv.requirements = ''
+      python-lsp-server
+    '';
+  };
+
+  pre-commit.hooks = {
+    commitizen.enable = true;
+  };
+
+  
+  enterShell = ''
+  '';
+}
---
+++ env.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# devenv --clean --nix-option pure-eval true shell
+devenv shell
